def4cdd57d77fec0d23494e9f1262f195802b142
Charu Thomas <charu.thomas@gmail.com>
2018-06-18 23:43:02 -0400

    added the cube and camera movement with Pramod

--- SceneKit Tutorial 1/ViewController.swift
+++ SceneKit Tutorial 1/ViewController.swift
@@ -7,12 +7,60 @@
 //
 
 import UIKit
+import SceneKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let sceneView = SCNView(frame: self.view.frame)
+        self.view.addSubview(sceneView)
+        
+        let scene = SCNScene()
+        sceneView.scene = scene
+        sceneView.allowsCameraControl = true
+        
+        let camera = SCNCamera()
+        let cameraNode = SCNNode()
+        cameraNode.camera = camera
+        cameraNode.position = SCNVector3(x: -3.0, y: 3.0, z: 3.0)
+        
+        let light = SCNLight()
+        light.type = SCNLight.LightType.spot
+        light.spotInnerAngle = 30.0
+        light.spotOuterAngle = 80.0
+        light.castsShadow = true
+        let lightNode = SCNNode()
+        lightNode.light = light
+        lightNode.position = SCNVector3(x: 1.5, y: 1.5, z: 1.5)
+        
+        let cubeGeometry = SCNBox(width: 1.0, height: 1.0, length: 1.0, chamferRadius: 0.0)
+        let cubeNode = SCNNode(geometry: cubeGeometry)
+        
+        let planeGeometry = SCNPlane(width: 50.0, height: 50.0)
+        let planeNode = SCNNode(geometry: planeGeometry)
+        planeNode.eulerAngles = SCNVector3(x: GLKMathDegreesToRadians(-90), y: 0, z: 0)
+        planeNode.position = SCNVector3(x:0, y:-0.5, z:0)
+        
+        let redMaterial = SCNMaterial()
+        redMaterial.diffuse.contents = UIColor.red
+        cubeGeometry.materials = [redMaterial]
+        
+        let greenMaterial = SCNMaterial()
+        greenMaterial.diffuse.contents = UIColor.green
+        planeGeometry.materials = [greenMaterial]
+        
+        let constraint = SCNLookAtConstraint(target: cubeNode)
+        constraint.isGimbalLockEnabled = true
+        cameraNode.constraints = [constraint]
+        lightNode.constraints = [constraint]
+        
+        scene.rootNode.addChildNode(lightNode)
+        scene.rootNode.addChildNode(cameraNode)
+        scene.rootNode.addChildNode(cubeNode)
+        scene.rootNode.addChildNode(planeNode)
     }
 
     override func didReceiveMemoryWarning() {
